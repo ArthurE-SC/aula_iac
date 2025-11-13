f14477f9c6c00502bf025221df6554de479c821c
Cynthia Adriane <arthurcegalin@gmail.com>
2025-11-13 20:44:55 -0300

    Envio dos arquivos

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function saudacao () {
+  const nome = "Aluno";
+  const data = new Date().toLocaleString("pt-BR");
+  console.log(`Olá, ${nome}!`);
+  console.log(`Este script está rodando dentro de um container Docker.`);
+  console.log(`Horário da execução: ${data}`);
+}
+
+saudacao();
---
+++ dockerfile
@@ -0,0 +1,11 @@
+
+FROM node:18
+
+# Define o diretório de trabalho dentro do container
+WORKDIR /usr/src/app
+
+# Copia o arquivo do projeto para dentro do container
+COPY app.js .
+
+# Define o comando a ser excecutado quando o container iniciar
+CMD ["node", "app.js"]
